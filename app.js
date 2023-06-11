72fc0b63ccf14a965ae74d184018a0f2a3519dde
Babak Jahedmanesh <babak.jahedmanesh@thenavpeople.co.uk>
2023-06-11 18:30:25 +0100

    complex async timing for events!

--- app.js
+++ app.js
@@ -4,14 +4,33 @@ class MyEmitter extends EventEmitter { }
 
 const myEmitter = new MyEmitter();
 myEmitter.on('event', (param) => {
-    param.a++;
-    console.log(`First handler: an event occurred! ${param.a}`);
+    setImmediate(() => {
+        param.a++;
+        console.log(`Zeroth handler(immidiate): an event occurred! ${param.a}`);
+    });
+});
+myEmitter.on('event', (param) => {
+    setTimeout(() => {
+        param.a++;
+        console.log(`First handler(3s): an event occurred! ${param.a}`);
+    }, 3000);
+});
+myEmitter.on('event', (param) => {
+    setTimeout(() => {
+        param.a++;
+        console.log(`Second handler(2s): an event occurred! ${param.a}`);
+    }, 2000);
+});
+myEmitter.on('event', (param) => {
+    setImmediate(() => {
+        param.a++;
+        console.log(`Third handler(immidiate): an event occurred! ${param.a}`);
+    });
 });
 myEmitter.on('event', (param) => {
     param.a++;
-    console.log(`Second handler: an event occurred! ${param.a}`);
+    console.log(`Last handler(no timer): an event occurred! ${param.a}`);
 });
-
 const eventParams = {};
 eventParams.a = 1;
 myEmitter.emit('event', eventParams);
